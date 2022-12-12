baaaaf54407a7d84ac831ce05f6b2c3d619acef9
Maximilien Zaleski <maximilienzaleski@yahoo.com>
2022-12-12 12:45:48 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# use-timer
+
+React hook which returns a timer's remaining span as a formatted time string, e.g. 65 -> "1:05", as well as numerically.
+
+## Table of Contents
+
+- [use-timer](#use-timer)
+  - [Table of Contents](#table-of-contents)
+  - [Getting Started](#getting-started)
+  - [Usage](#usage)
+    - [Hook Returned Properties](#hook-returned-properties)
+    - [Hook Options](#hook-options)
+  - [TypeScript Support](#typescript-support)
+  - [License](#license)
+
+---
+
+## Getting Started 
+
+Install through your package manager of choice (npm, yarn, etc.)
+
+```
+npm -i @mz/use-timer
+```
+
+```
+yarn add @mz/use-timer
+```
+
+## Usage
+
+```jsx
+import { useTimer } from '@mz/use-timer';
+
+function Component(props) {
+  const { timeRemaining, secondsRemaining, setFreeze, resetTimer } = useTimer(3500, false,                               
+    () => console.log('Timer finished!') 
+  );
+
+  return (
+    <div>
+      <p>Time remaining: {timeRemaining}</p>
+      <p>Seconds remaining: {secondsRemaining}</p>
+      <button onClick={() => setFreeze(true)}>Freeze</button>
+      <button onClick={() => setFreeze(false)}>Unfreeze</button>
+      <button onClick={() => resetTimer()}>Reset</button>
+    </div>
+  );
+}
+```
+
+### Hook Returned Properties
+
+| Name               | Description                                                                                                                 |
+| ------------------ | --------------------------------------------------------------------------------------------------------------------------- |
+| `timeRemaining`    | The time remaining as a formatted string e.g., "3:15"                                                                       |
+| `secondsRemaining` | The time remaining as seconds                                                                                               |
+| `isFrozen`         | Whether the timer is frozen                                                                                                 |
+| `setFreeze`        | A setter for the timer's freeze state                                                                                       |
+| `resetTimer`       | A function to force-reset the timer – takes the `freeze` parameter which dictates whether to start the timer upon reset. |
+
+### Hook Options 
+
+| Name             | Description                                |
+| ---------------- | ------------------------------------------ |
+| `initialSeconds` | Initial (remaining) time in seconds        |
+| `initialFreeze`  | Initial freeze state                       |
+| `onCompleted`    | A hook called once the timer has completed |
+
+## TypeScript Support 
+
+You will find a collection of typings bundled with the package.
+
+## License
+
+[MIT License](LICENSE) (c) 2022 Maximilien Zaleski
---
+++ src/index.ts
@@ -0,0 +1,102 @@
+/**
+MIT License
+
+Copyright (c) 2022 Maximilien Zaleski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+import React from 'react';
+
+export interface UseTimerState {
+  /** The time remaining as a formatted string. */
+  timeRemaining: string;
+  /** The time remaining as seconds. */
+  secondsRemaining: number;
+
+  /** Whether the timer should count down. */ 
+  setFreeze(value: boolean): void;
+  /** Force the timer to reset.  */
+  resetTimer(freeze?: boolean): void;
+}
+
+/**
+ * useTimer is a hook that spawns a timer which ticks every second.
+ *
+ * @param initialSeconds - Initial (remaining) time in seconds. 
+ * @param initialFreeze - Initial freeze state; if true, the timer will not tick.
+ * @param onCompleted - Hook called when timer completes.
+ */
+export function useTimer(
+  initialSeconds: number,
+  initialFreeze: boolean,
+  onCompleted?: () => void
+): UseTimerState {
+  const [secondsRemaining, setSecondsRemaining] = React.useState(initialSeconds);
+  const [timeRemaining, setTimeRemaining] = React.useState('');
+  const [freeze, setFreeze] = React.useState(initialFreeze);
+
+  React.useEffect(() => setSecondsRemaining(initialSeconds), [initialSeconds]);
+
+  React.useEffect(() => {
+    // Make the timer tick every second.
+    if (secondsRemaining > 0 && !freeze) {
+      const timeout = setTimeout(() => {
+        const hydrated = secondsRemaining - 1;
+        setTimeRemaining(calculateTimeRemaining(hydrated));
+        setSecondsRemaining(hydrated);
+      }, 1000); // 1 second.
+      return () => clearTimeout(timeout);
+    // Countdown has been reached -> reset timer. 
+    } else if (secondsRemaining === 0) {
+      setFreeze(true);
+      // Call `onCompleted` hook if provided. 
+      if (onCompleted) onCompleted();
+    }
+  }, [freeze, initialSeconds, onCompleted, secondsRemaining]);
+
+  const resetTimer = React.useCallback((freeze: boolean = false) => {
+    setSecondsRemaining(initialSeconds);
+    setTimeRemaining(calculateTimeRemaining(initialSeconds));
+    setFreeze(freeze);
+  }, [initialSeconds])
+
+  return {
+    timeRemaining,
+    secondsRemaining,
+    setFreeze,
+    resetTimer,
+  } as UseTimerState;
+}
+
+/**
+ * calculateTimeRemaining returns a formatted string of the time remaining.
+ *
+ * If the time is less than a minute, it will return the time in seconds.
+ *
+ * @param secondsRemaining - The number of seconds remaining.
+ */
+function calculateTimeRemaining(secondsRemaining: number) {
+  if (secondsRemaining < 60) return secondsRemaining.toString();
+  else {
+    const seconds = secondsRemaining % 60;
+    const minutes = (secondsRemaining - seconds) / 60;
+    return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+  }
+}
